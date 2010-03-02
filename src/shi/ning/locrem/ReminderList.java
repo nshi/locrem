e058e9368baa87bcc80c41d21323407129110310
Ning Shi <zeegeek@gmail.com>
2010-03-02 10:59:03 -0500

    Adds missing source file.

---
+++ src/shi/ning/locrem/ReminderList.java
@@ -0,0 +1,12 @@
+package shi.ning.locrem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReminderList extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
