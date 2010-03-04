5a349d8fe997d7c35c4e4f91a93c36b10e5b1571
Ning Shi <zeegeek@gmail.com>
2010-03-04 18:27:07 -0500

    Adds skeleton of the ReminderEntry class.

---
+++ src/shi/ning/locrem/ReminderEntry.java
@@ -0,0 +1,17 @@
+package shi.ning.locrem;
+
+import java.util.List;
+
+import android.location.Address;
+import android.text.format.Time;
+
+public class ReminderEntry {
+    private long mId;
+    private Time mTime;
+    private List<Address> mAddress;
+
+    public ReminderEntry() {
+
+    }
+
+}
