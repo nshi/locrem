403bdd7de085d63e6fca0988fe565a47b8a20b92
Ning Shi <zeegeek@gmail.com>
2010-03-07 14:54:16 -0500

    Use Parcel to marshall ReminderEntry object.

--- src/shi/ning/locrem/ReminderEntry.java
+++ src/shi/ning/locrem/ReminderEntry.java
@@ -3,14 +3,34 @@ package shi.ning.locrem;
 import java.util.List;
 
 import android.location.Address;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.format.Time;
 
-public final class ReminderEntry {
+public final class ReminderEntry implements Parcelable {
     private long mId;
     private Time mTime;
     private Time mLastCheck;
     private String mLocation;
     private List<Address> mAddresses;
+
+    private ReminderEntry(Parcel in) {
+        mId = in.readLong();
+        long time = in.readLong();
+        mTime = null;
+        if (time > 0) {
+            mTime = new Time();
+            mTime.set(time);
+        }
+        time = in.readLong();
+        mLastCheck = null;
+        if (time > 0) {
+            mLastCheck = new Time();
+            mLastCheck.set(time);
+        }
+        mLocation = in.readString();
+        in.readTypedList(mAddresses, null);
+    }
 
     public ReminderEntry(String location) {
         this(-1, null, location);
@@ -66,5 +86,37 @@ public final class ReminderEntry {
 
     public void setAddresses(List<Address> addresses) {
         mAddresses = addresses;
+    }
+
+    public static final Parcelable.Creator<ReminderEntry> CREATE = new Parcelable.Creator<ReminderEntry>() {
+        @Override
+        public ReminderEntry createFromParcel(Parcel source) {
+            return new ReminderEntry(source);
+        }
+
+        @Override
+        public ReminderEntry[] newArray(int size) {
+            return new ReminderEntry[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mId);
+        if (mTime != null)
+            dest.writeLong(mTime.toMillis(false));
+        else
+            dest.writeLong(0);
+        if (mLastCheck != null)
+            dest.writeLong(mLastCheck.toMillis(false));
+        else
+            dest.writeLong(0);
+        dest.writeString(mLocation);
+        dest.writeTypedList(mAddresses);
     }
 }
